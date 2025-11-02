e36a9768e2ca719bd857f4ee0b4f6368ee3d8f14
TajemulCSE <tajemulislamgames@gmail.com>
2025-11-02 11:41:36 +0600

    Add:Alarm in Home screen

---
+++ lib/features/home/presentation/pages/alarm_list_widget.dart
@@ -0,0 +1,149 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class AlarmListWidget extends StatefulWidget {
+  const AlarmListWidget({super.key});
+
+  @override
+  State<AlarmListWidget> createState() => _AlarmListWidgetState();
+}
+
+class _AlarmListWidgetState extends State<AlarmListWidget> {
+  List<Map<String, dynamic>> alarms = [
+    {'time': '7:10 pm', 'date': 'Fri 21 Mar 2025', 'enabled': true},
+    {'time': '6:55 pm', 'date': 'Fri 21 Mar 2025', 'enabled': false},
+    
+  ];
+
+  Future<void> _pickDateTime() async {
+    DateTime now = DateTime.now();
+
+    final DateTime? date = await showDatePicker(
+      context: context,
+      initialDate: now,
+      firstDate: now,
+      lastDate: DateTime(2030),
+      builder: (context, child) => Theme(
+        data: ThemeData.dark(),
+        child: child!,
+      ),
+    );
+
+    if (date == null) return;
+
+    final TimeOfDay? time = await showTimePicker(
+      context: context,
+      initialTime: TimeOfDay.now(),
+      builder: (context, child) => Theme(
+        data: ThemeData.dark(),
+        child: child!,
+      ),
+    );
+
+    if (time == null) return;
+
+    final DateTime finalDateTime = DateTime(
+      date.year,
+      date.month,
+      date.day,
+      time.hour,
+      time.minute,
+    );
+
+    final formattedTime = DateFormat.jm().format(finalDateTime);
+    final formattedDate = DateFormat('EEE d MMM yyyy').format(finalDateTime);
+
+    setState(() {
+      alarms.add({
+        'time': formattedTime,
+        'date': formattedDate,
+        'enabled': true,
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        ListView.builder(
+          padding: const EdgeInsets.all(16),
+          itemCount: alarms.length,
+          itemBuilder: (context, index) {
+            final alarm = alarms[index];
+            return Container(
+              margin: const EdgeInsets.only(bottom: 12),
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+              decoration: BoxDecoration(
+                color: const Color(0xFF1B0F39),
+                borderRadius: BorderRadius.circular(89),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  // Time and Date
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        alarm['time'],
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontFamily: 'popins',
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      const SizedBox(width: 80),
+                      Text(
+                        alarm['date'],
+                        style: TextStyle(
+                          color: Colors.white54,
+                          fontFamily: 'popins',
+                          
+                          fontSize: 14,
+                        ),
+                      ),
+                    ],
+                  ),
+
+                  // Switch
+                  Switch(
+                    value: alarm['enabled'],
+                    onChanged: (value) {
+                      setState(() {
+                        alarms[index]['enabled'] = value;
+                      });
+                    },
+                    
+                    activeTrackColor: Color(0xFF5200FF),
+                    inactiveThumbColor: Colors.black,
+                    inactiveTrackColor: Colors.white,
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
+
+        // Floating Action Button
+        Positioned(
+          bottom: 60,
+          right: 20,
+          child: SizedBox(
+            height: 66,
+            width: 66,
+            child: FloatingActionButton(
+              
+              elevation: 0,shape: CircleBorder(),
+              backgroundColor: Color(0xFF5200FF),
+              onPressed: _pickDateTime,
+              child: const Icon(Icons.add, color: Colors.white),
+              
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/features/home/presentation/screens/home_screen.dart
+++ lib/features/home/presentation/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:smart_travel_alarm/features/home/presentation/pages/alarm_list_widget.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -13,19 +14,73 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       backgroundColor: Colors.transparent,
       body: Container(
-        height: double.infinity,
         width: double.infinity,
-        decoration: BoxDecoration(
+        height: double.infinity,
+        decoration: const BoxDecoration(
           gradient: LinearGradient(
-            colors: [ Color(0xFF0B0024),Color(0xFF082257)],
+            colors: [Color(0xFF0B0024), Color(0xFF082257)],
             begin: Alignment.topCenter,
             end: Alignment.bottomCenter,
           ),
         ),
-        child: Stack(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-           
-            
+            // Location
+            Padding(
+              padding: const EdgeInsets.only(top: 80, left: 20, right: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    "Selected Location",
+                    style: TextStyle(
+                      fontFamily: 'popins',
+                      color: Colors.white,
+                      fontSize: 20,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+
+                  //location text field
+                  Container(
+                    height: 66,
+                    decoration: BoxDecoration(
+                      color: const Color(0xFF1B0F39),
+                      borderRadius: BorderRadius.circular(61),
+                    ),
+                    child: const Center(
+                      child: Padding(
+                        padding: EdgeInsets.all(15.0),
+                        child: TextField(
+                          style: TextStyle(
+                            color: Colors.white54,
+                            fontFamily: 'popins',
+                          ),
+                          decoration: InputDecoration(
+                            prefixIcon: ImageIcon(
+                              AssetImage('assets/icons/location_1.png'),
+                              color: Colors.white70,
+                            ),
+                            hintText: 'Add your location',
+                            hintStyle: TextStyle(
+                              color: Colors.white54,
+                              fontSize: 18,
+                              fontFamily: 'popins',
+                            ),
+                            border: InputBorder.none,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            //alarm list
+            const SizedBox(height: 20),
+            const Expanded(child: AlarmListWidget()),
           ],
         ),
       ),
